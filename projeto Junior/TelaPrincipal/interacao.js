1b08a6543962d9a6419ad53a09e5569721d38dc8
savio-cell <117689480+savio-cell@users.noreply.github.com>
2023-07-31 20:38:21 -0300

    Add files via upload

---
+++ projeto Junior/TelaPrincipal/interacao.js
@@ -0,0 +1,6 @@
+// 1 obejetivo = mult a  quantia de quantos lanches vao pedir 
+// 1- passo pegar a tag (p) e fazer um loop paara sempre somar mais um ate 10
+    while(let = i = 0);
+    if (i < 10) {
+        console.log(i)++;
+    }
